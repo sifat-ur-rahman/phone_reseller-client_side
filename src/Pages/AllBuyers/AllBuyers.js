4eefd300f5acce6ff5f388e2e22eea5847944001
sifat-ur-rahman <md.sifat.ur.rahman2702@gmail.com>
2022-11-27 09:50:26 +0600

    add

--- src/Pages/AllBuyers/AllBuyers.js
+++ src/Pages/AllBuyers/AllBuyers.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 const AllBuyers = () => {
+    const {data: users = [],} = useQuery({
+        queryKey: ['users'],
+        queryFn: async() =>{
+            const res = await fetch('http://localhost:5000/buyer')
+            const data = await res.json()
+            return data
+        }
+    })
+    console.log(users);
     return (
         <div>
-            <h2>All Buyers</h2>
+            <h2 className='text-3xl'> All Buyers</h2>
+            <div className="overflow-x-auto">
+  <table className="table w-full">
+    
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Email</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      
+      {
+        users.map((user, i) => <tr key={user._id} className="hover">
+        <th>{i+1}</th>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        
+      </tr>)
+      }
+      
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
