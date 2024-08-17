ffc70b7e2cba519572a046363642ce02a3b63792
sifat <md.sifat.ur.rahman2702@gmail.com>
2024-08-17 19:06:12 +0600

    add

--- src/Pages/Home/Environmental/Environmental.js
+++ src/Pages/Home/Environmental/Environmental.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React from "react";
+import img01 from "../../../../src/images/Recycle-Cell-Phones.jpeg";
 
 const Environmental = () => {
-    return (
-        <div>
-            <div className="hero min-h-screen ">
-  <div className="hero-content flex-col lg:flex-row">
-    <img src="https://i.ibb.co/vv8YsG8/RD-Recycle-Cell-Phones-Sustainability-Getty-Images-3-JVedit.jpg" className=" rounded-lg lg:w-1/2  " alt='' />
+  return (
     <div>
-      <h1 className="text-5xl font-bold">Sell your old electronics products to reduce environmental pollution!</h1>
-      <ul className="py-6 text-xl">
-        <li>Find a reputable buyer for your old electronics to ensure they are properly disposed of or recycled.</li>
-        <li>Before selling, make sure to erase all personal data from your device to protect your privacy.</li>
-        <li>Research the value of your old electronics to ensure you get a fair price when selling.</li>
-        <li>Always prioritize selling over throwing away old electronics to minimize environmental pollution.</li>
-      </ul>
-      
-    </div>
-  </div>
-</div>
+      <div className="hero min-h-screen mx-auto">
+        <div className="hero-content flex-col lg:flex-row ">
+          <img src={img01} className=" rounded-lg  w-[100%] " alt="" />
+          <div>
+            <h1 className="text-5xl font-bold">
+              Sell your old electronics products to reduce environmental
+              pollution!
+            </h1>
+            <ul className="py-6 text-xl">
+              <li>
+                Find a reputable buyer for your old electronics to ensure they
+                are properly disposed of or recycled.
+              </li>
+              <li>
+                Before selling, make sure to erase all personal data from your
+                device to protect your privacy.
+              </li>
+              <li>
+                Research the value of your old electronics to ensure you get a
+                fair price when selling.
+              </li>
+              <li>
+                Always prioritize selling over throwing away old electronics to
+                minimize environmental pollution.
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Environmental;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import Affordable from '../Affordable/Affordable';
-import Carousel from '../Carousel/Carousel';
-import Category from '../Category/Category';
-import Contact from '../Contact/Contact';
-import Discount from '../Discount/Discount';
-import Environmental from '../Environmental/Environmental';
-import BestSale from '../BestSale/BestSale';
-import BestSeller from '../BestSeller/BestSeller';
-import FAQ from '../FAQ/FAQ';
-import Capturing from '../Capturing/Capturing';
-import Advertise from '../Advertise/Advertise';
-import Captivating from '../Captivating/Captivating';
-
+import React from "react";
+import Affordable from "../Affordable/Affordable";
+import Carousel from "../Carousel/Carousel";
+import Category from "../Category/Category";
+import Contact from "../Contact/Contact";
+import Discount from "../Discount/Discount";
+import Environmental from "../Environmental/Environmental";
+import BestSale from "../BestSale/BestSale";
+//import BestSeller from "../BestSeller/BestSeller";
+import FAQ from "../FAQ/FAQ";
+import Capturing from "../Capturing/Capturing";
+import Advertise from "../Advertise/Advertise";
+import Captivating from "../Captivating/Captivating";
 
 const Home = () => {
-    return (
-        <div>
-            <Carousel></Carousel>
-           <Affordable></Affordable>
-           <Discount></Discount>
-           <Captivating></Captivating>
-           <Category></Category>
-           <Environmental></Environmental>
-           <BestSale></BestSale>
-           <Capturing></Capturing>
-           <BestSeller></BestSeller>
-           <FAQ></FAQ>
-           <Advertise></Advertise>
-           <Contact></Contact>
-        </div>
-    );
+  return (
+    <div>
+      <Carousel></Carousel>
+      <Affordable></Affordable>
+      <Discount></Discount>
+      <Captivating></Captivating>
+      <Category></Category>
+      <Environmental></Environmental>
+      <BestSale></BestSale>
+      <Capturing></Capturing>
+      {/* <BestSeller></BestSeller> */}
+      <FAQ></FAQ>
+      <Advertise></Advertise>
+      <Contact></Contact>
+    </div>
+  );
 };
 
 export default Home;
