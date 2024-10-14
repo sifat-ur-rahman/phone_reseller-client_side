56e0f8a178c391b33c767ccf6f86db239723c948
sifat <md.sifat.ur.rahman2702@gmail.com>
2024-10-14 17:11:13 +0600

    add

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,28 +1,62 @@
-import React from 'react';
+import React from "react";
 
 const Blogs = () => {
-    return (
-        <div>
-            <div className='ml-5 mr-5 rounded bg-lime-300 p-4 mb-5'>
-                <h4 className='text-xl'> What are the different ways to manage a state in a React application?</h4>
-                <p>There are four main types of state you need to properly manage in your React apps: <br/> 1.Local state,<br/> 2.Global state, <br/>3.Server state,<br/> 4.URL state</p>
-            </div>
-            <div className='ml-5 mr-5 rounded bg-lime-300 p-4 mb-5'>
-                <h4 className='text-xl'> How does prototypical inheritance work?</h4>
-                <p>The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object.</p>
-            </div>
-            <div className='ml-5 mr-5 rounded bg-lime-300 p-4 mb-5'>
-                <h4 className='text-xl'>Why should we write unit tests?</h4>
-                <p>Unit testing ensures that all code meets quality standards before it's deployed. This ensures a reliable engineering environment where quality is paramount. Over the course of the product development life cycle, unit testing saves time and money, and helps developers write better code, more efficiently.</p>
-            </div>
-            <div className='ml-5 mr-5 rounded bg-lime-300 p-4 mb-5'>
-                <h4 className='text-xl'> React vs. Angular vs. Vue?</h4>
-                <p>React can be used as a UI library to render elements, without enforcing a specific project structure, and that’s why it’s not strictly a framework.</p>
-                <p>AngularJS, the original framework, is an MVC (Model-View-Controller) framework. But in Angular 2, there’s no strict association with MV*-patterns as it is also component-based.</p>
-                <p>The Vue.js core library focuses on the View layer only. It’s called a progressive framework because you can extend its functionality with official and third-party packages, such as Vue Router or Vuex, to turn it into an actual framework.</p>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="ml-5 mr-5 rounded  p-4 mb-5">
+        <h4 className="text-xl">
+          {" "}
+          What are the different ways to manage a state in a React application?
+        </h4>
+        <p>
+          There are four main types of state you need to properly manage in your
+          React apps: <br /> 1.Local state,
+          <br /> 2.Global state, <br />
+          3.Server state,
+          <br /> 4.URL state
+        </p>
+      </div>
+      <div className="ml-5 mr-5 rounded  p-4 mb-5">
+        <h4 className="text-xl"> How does prototypical inheritance work?</h4>
+        <p>
+          The Prototypal Inheritance is a feature in javascript used to add
+          methods and properties in objects. It is a method by which an object
+          can inherit the properties and methods of another object.
+          Traditionally, in order to get and set the [[Prototype]] of an object,
+          we use Object. getPrototypeOf and Object.
+        </p>
+      </div>
+      <div className="ml-5 mr-5 rounded  p-4 mb-5">
+        <h4 className="text-xl">Why should we write unit tests?</h4>
+        <p>
+          Unit testing ensures that all code meets quality standards before it's
+          deployed. This ensures a reliable engineering environment where
+          quality is paramount. Over the course of the product development life
+          cycle, unit testing saves time and money, and helps developers write
+          better code, more efficiently.
+        </p>
+      </div>
+      <div className="ml-5 mr-5 rounded  p-4 mb-5">
+        <h4 className="text-xl"> React vs. Angular vs. Vue?</h4>
+        <p>
+          React can be used as a UI library to render elements, without
+          enforcing a specific project structure, and that’s why it’s not
+          strictly a framework.
+        </p>
+        <p>
+          AngularJS, the original framework, is an MVC (Model-View-Controller)
+          framework. But in Angular 2, there’s no strict association with
+          MV*-patterns as it is also component-based.
+        </p>
+        <p>
+          The Vue.js core library focuses on the View layer only. It’s called a
+          progressive framework because you can extend its functionality with
+          official and third-party packages, such as Vue Router or Vuex, to turn
+          it into an actual framework.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
--- src/Pages/Home/Category/CategoryCard.js
+++ src/Pages/Home/Category/CategoryCard.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const CategoryCard = ({categori}) => {
-    const { category,img, describe,} = categori
-    return (
-        <div>
-            <div className="card border border-gray-100 hover:border-cyan-600 transition duration-300 ease-in-out  shadow-xl">
-  <figure className="">
-    <img src={img} alt="Shoes" className="rounded-xl w-50 h-60" />
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">Category: {category} Phone</h2>
-    <p>{describe}</p>
-    <div className="card-actions">
-      <Link to={`/category/${category}`}><button className="btn btn-outline btn-primary px-16 text-xl font-bold rounded-full">See More</button></Link>
-      
-    </div>
-  </div>
-</div>
+const CategoryCard = ({ categori }) => {
+  const { category, img, describe } = categori;
+  return (
+    <div>
+      <div className="card border border-gray-100 hover:border-cyan-600 transition duration-300 ease-in-out  shadow-xl">
+        <figure className="">
+          <img src={img} alt="Shoes" className="rounded-xl w-50 mt-3 h-60" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Category: {category} Phone</h2>
+          <p>{describe}</p>
+          <div className="card-actions">
+            <Link to={`/category/${category}`}>
+              <button className="btn btn-outline btn-primary px-16 text-xl font-bold rounded-full">
+                See More
+              </button>
+            </Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CategoryCard;
