d0a57e5e2a13ded76ab3f12ec8de5927d520cf41
sifat <md.sifat.ur.rahman2702@gmail.com>
2024-10-29 21:53:20 +0600

    add

--- src/Pages/Home/Category/Category.js
+++ src/Pages/Home/Category/Category.js
@@ -1,47 +1,37 @@
-import React from 'react';
-import CategoryCard from './CategoryCard';
+import React from "react";
+import CategoryCard from "./CategoryCard";
 
 const Category = () => {
-    const categoryData = [
-        {
-            id : 1,
-            category : '3G',
-            describe : 'If you want 3G phone',
-            img: 'https://i.ibb.co/BLHZ7L1/images.jpg',
-            
-
-        },
-        {
-            id : 2,
-            category : '4G',
-            describe : 'If you want 4G phone',
-            img: 'https://i.ibb.co/fv2m3Lb/images-1.jpg',
-
-
-        },
-        {
-            id : 3,
-            category : '5G',
-            describe : 'If you want 5G phone',
-            img: 'https://i.ibb.co/PC9Mr4s/images-2.jpg',
-
-
-        }
-    ]
-    return (
-        <div className='mt-16 mb-8'>
-            <h2 className='text-4xl font-extrabold  text-center'>Phone Category</h2>
-            <div className='grid mx-8 my-8 gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-            {
-               categoryData.map((categori)=> <CategoryCard
-               key={categori.id}
-               categori = {categori}
-               ></CategoryCard>) 
-            }
-
-            </div>
-        </div>
-    );
+  const categoryData = [
+    {
+      id: 1,
+      categoryName: "3G",
+      describe: "If you want 3G phone",
+      img: "https://i.ibb.co/BLHZ7L1/images.jpg",
+    },
+    {
+      id: 2,
+      categoryName: "4G",
+      describe: "If you want 4G phone",
+      img: "https://i.ibb.co/fv2m3Lb/images-1.jpg",
+    },
+    {
+      id: 3,
+      categoryName: "5G",
+      describe: "If you want 5G phone",
+      img: "https://i.ibb.co/PC9Mr4s/images-2.jpg",
+    },
+  ];
+  return (
+    <div className="mt-16 mb-8">
+      <h2 className="text-4xl font-extrabold  text-center">Phone Category</h2>
+      <div className="grid mx-8 my-8 gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {categoryData.map((category) => (
+          <CategoryCard key={category.id} category={category}></CategoryCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Category;
--- src/Pages/Home/Category/CategoryCard.js
+++ src/Pages/Home/Category/CategoryCard.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const CategoryCard = ({ categori }) => {
-  const { category, img, describe } = categori;
+const CategoryCard = ({ category }) => {
+  const { categoryName, img, describe } = category;
   return (
-    <div>
+    <div className="container ">
       <div className="card border border-gray-100 hover:border-cyan-600 transition duration-300 ease-in-out  shadow-xl">
         <figure className="">
-          <img src={img} alt="Shoes" className="rounded-xl w-50 mt-3 h-60" />
+          <img src={img} alt="Shoes" className="rounded-xl w-44 mt-3 h-52" />
         </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Category: {category} Phone</h2>
-          <p>{describe}</p>
+        <div className="p-5 grid items-center justify-center text-center">
+          <h2 className="text-2xl font-semibold">
+            Category: {categoryName} Phone
+          </h2>
+          <p className="pt-1 pb-3">{describe}</p>
           <div className="card-actions">
-            <Link to={`/category/${category}`}>
+            <Link to={`/category/${categoryName}`}>
               <button className="btn btn-outline btn-primary px-16 text-xl font-bold rounded-full">
                 See More
               </button>
