c9a6dfdff08d643b4c1ae997bcf37ac22f78dd46
sifat <md.sifat.ur.rahman2702@gmail.com>
2024-10-29 21:22:49 +0600

    add

--- src/Pages/Home/Carousel/Carousel.js
+++ src/Pages/Home/Carousel/Carousel.js
@@ -1,38 +1,48 @@
-import React from 'react';
-import img1 from '../../../phone/img1.jpg'
-import img2 from '../../../phone/img2.jpg'
-import img3 from '../../../phone/img3.jpg'
-
+import React from "react";
+import img1 from "../../../phone/img1.jpg";
+import img2 from "../../../phone/img2.jpg";
+import img3 from "../../../phone/img3.jpg";
 
 const Carousel = () => {
-    return (
-        <div>
-            <div className="carousel max-h-72 w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img src={img1} alt='' className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img src={img2} alt='' className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src={img3} alt='' className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  
-</div>
+  return (
+    <div>
+      <div className="carousel max-h-96 w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={img1} alt="" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
-    );
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={img2} alt="" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={img3} alt="" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
