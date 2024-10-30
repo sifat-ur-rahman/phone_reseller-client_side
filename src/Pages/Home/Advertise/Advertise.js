428e19f2c03526b8f42c8dc0763721b8ab439fd4
sifat <md.sifat.ur.rahman2702@gmail.com>
2024-10-30 10:40:22 +0600

    add

--- src/Pages/Home/Advertise/Advertise.js
+++ src/Pages/Home/Advertise/Advertise.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const Advertise = () => {
-    return (
-        <div>
-        
-        <div className="hero mt-4 mb-5">
-<div className="hero-content flex-col lg:flex-row-reverse">
-
-<div className='px-5'>
-  <h1 className="text-5xl font-bold">Before diving into the resale process, it's essential to evaluate the condition of your phone.</h1>
-  <p className="py-6 text-xl font-semibold">Factors such as physical appearance, functionality, battery life, and any included accessories can significantly impact its resale value. Take a close look at your phone and make note of any cosmetic or functional issues. Being honest and transparent about your device's condition will ensure a fair transaction and help you set the right price.</p>
-  
-</div>
-<img src='https://i.ibb.co/sq5n3qM/11622059775-0.jpg'className="w-full lg:w-1/2 rounded-lg shadow-2xl" alt='' />
-
-</div>
-</div>
+  return (
+    <div>
+      <div className="container mx-auto mt-14 mb-5">
+        <div className="grid grid-cols-2 gap-6 items-center">
+          <div className="px-5">
+            <h1 className="text-3xl font-bold">
+              Before diving into the resale process, it's essential to evaluate
+              the condition of your phone.
+            </h1>
+            <p className="py-6 text-base">
+              Physical appearance, functionality, battery life, and accessories
+              greatly influence resale value. Carefully check for any cosmetic
+              or functional issues, and be transparent about the phone's
+              condition. This honesty ensures fair pricing and builds trust with
+              buyers.
+            </p>
+          </div>
+          <img
+            src="https://i.ibb.co/sq5n3qM/11622059775-0.jpg"
+            className="w-full  rounded-lg shadow-2xl"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Advertise;
