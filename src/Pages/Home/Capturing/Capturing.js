6ecdbd02df5e0ed2be21ba3da928e19140fc8a1c
sifat <md.sifat.ur.rahman2702@gmail.com>
2024-10-30 10:36:37 +0600

    add

--- src/Pages/Home/Capturing/Capturing.js
+++ src/Pages/Home/Capturing/Capturing.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const Capturing = () => {
-    return (
-        <div>
-        
-        <div className="hero mt-4 mb-5">
-<div className="hero-content flex-col lg:flex-row-reverse">
-<img src='https://i.ibb.co/0f5FKb4/DLJFtt-Mg-LMb-NSsr9-Rx-P8-HX-1200-80.jpg' className="w-full lg:w-1/2 rounded-lg shadow-2xl" alt='' />
-<div className='px-5'>
-  <h1 className="text-5xl font-bold">Capturing Eye-Catching Photos and Writing a Compelling Description</h1>
-  <p className="py-6 text-xl font-semibold">When listing your phone for resale, high-quality photos are essential to make a good first impression. Ensure your images are well-lit, showcase the phone from various angles, and highlight any unique features or accessories included. In your description, be honest and provide accurate details about the phone's specifications, condition, and any remaining warranty. Clearly state what is included in the sale, such as charging cables, original packaging, or additional accessories. Honesty and transparency go a long way in building trust with potential buyers.</p>
-  
-</div>
-</div>
-</div>
+  return (
+    <div>
+      <div className="container mx-auto mt-14 mb-5">
+        <div className="grid lg:grid-cols-2 gap-6 items-center">
+          <div className="px-5">
+            <h1 className="text-3xl font-bold">
+              Capturing Eye-Catching Photos and Writing a Compelling Description
+            </h1>
+            <p className="py-6 text-base">
+              To resell your phone, take high-quality, well-lit photos from
+              multiple angles to showcase its condition and features. In the
+              description, be honest about specifications, condition, and any
+              warranty. List included items like cables or packaging.
+              Transparency builds trust with buyers.
+            </p>
+          </div>
+          <img
+            src="https://i.ibb.co/0f5FKb4/DLJFtt-Mg-LMb-NSsr9-Rx-P8-HX-1200-80.jpg"
+            className="w-full  rounded-lg shadow-2xl"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Capturing;
